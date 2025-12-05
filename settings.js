cfffb76c78f38f93d6240c8546e18dcd52940488
artexpury925 <artexpury@gmail.com>
2025-12-05 10:19:53 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Muzan Bot',
+  author: 'Arnold Chirchir',
+  botName: "Muzan Bot",
+  botOwner: 'Arnold Chirchir', // Your name
+  ownerNumber: '254703110780', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/artexpury925/MUZAN-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
